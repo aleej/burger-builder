92e433f617711ef7126cc1650cd227d37694c364
Anthony Lee <anthony_lee@bcbsil.com>
2020-02-06 09:32:37 -0600

    Task#181 Adds responsive hamburger menu for sidecar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,19 +7,22 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
 
+    sideDrawerOpenedHandler = () => {
+        this.setState({showSideDrawer: true});
+    }
+
     render() {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar openSideDrawer={this.sideDrawerOpenedHandler} />
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
-                <div>Toolbar, SideDrawer, Backdrop</div>
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
